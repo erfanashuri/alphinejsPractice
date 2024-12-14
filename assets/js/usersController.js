8c07b4bf443594f7924b35d0b9550f8f8fc83254
erfanashuri <ashoryerfan@yahoo.com>
2024-12-14 21:02:58 +0330

    page show/change handlers added.

--- assets/js/usersController.js
+++ assets/js/usersController.js
@@ -1,12 +1,12 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('usersData', () => ({
         users: [],
+        pageUsers:[],
         showUsersError: false,
         showUsersLoading: true,
-        itemsCount: 5,
-        pagesCount: 0,
-        currentPage: 2,
-        
+        itemsCount: 3,
+        pagesCount: null,
+        currentPage: 1,
         getUsers(){
             setTimeout(()=>{
             fetch('https://jsonplaceholder.typicode.com/users')
@@ -26,10 +26,25 @@ document.addEventListener('alpine:init', () => {
             },1000)
         },
         paginationHandler(){
-            this.pagesCount = Math.ceil(this.users.length/this.itemsCount);
             let fItemIndex = (this.currentPage*this.itemsCount)-this.itemsCount;
             let lItemIndex = this.currentPage*this.itemsCount;
-            this.users=this.users.slice(fItemIndex,lItemIndex);
+            this.pageUsers=this.users.slice(fItemIndex,lItemIndex);
+            console.log(this.currentPage);
+        },
+        pageChanger(action){
+            action==='next'?this.currentPage++:this.currentPage--;
+            this.pagesCount = Math.ceil(this.users.length/this.itemsCount);
+            if(this.currentPage>this.pagesCount){this.currentPage = this.pagesCount};
+            if(this.currentPage<1){this.currentPage = 1};
+            this.paginationHandler();
+        },
+        perPageHandler(event){
+            event.target.value>this.users.length?event.target.value = this.users.length
+            :event.target.value<1?event.target.value = 1
+            :event.target.value=Math.ceil(event.target.value);
+            this.itemsCount=event.target.value;
+            this.currentPage=1;
+            this.paginationHandler();
         }
     }))
 })
